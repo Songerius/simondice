56954e4d0a39d4dafae3c0a3d6d336f0c3c871b9
Songer <ramosger_14@hotmail.com>
2020-11-19 18:59:10 -0600

    agregando las funciones para Iluminar secuencias

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
-const celeste = document.getElementById('celeste')
-const violeta = document.getElementById('violeta')
-const naranja = document.getElementById('naranja')
-const verde = document.getElementById('verde')
-const btnEmpezar = document.getElementById('btnEmpezar');
+const celeste = document.getElementById("celeste");
+const violeta = document.getElementById("violeta");
+const naranja = document.getElementById("naranja");
+const verde = document.getElementById("verde");
+const btnEmpezar = document.getElementById("btnEmpezar");
 
 class Juego {
-    constructor() {
-        this.inicializar()
-        this.generarSecuencia()
+  constructor() {
+    this.inicializar();
+    this.generarSecuencia();
+    this.siguienteNivel();
+  }
+  inicializar() {
+    btnEmpezar.classList.add("hide");
+    this.level = 1
+    this.colores = {
+      celeste,
+      violeta,
+      naranja,
+      verde,
+    };
+  }
+  generarSecuencia() {
+    this.secuencia = new Array(10)
+      .fill(0)
+      .map((n) => Math.floor(Math.random() * (4 - 1) + 1));
+  }
+  siguienteNivel() {
+    this.iluminarSecuencia();
+  }
+  transformarNumeroAcolor(numero) {
+    switch (numero) {
+      case 0:
+        return "celeste";
+      case 1:
+        return "violeta";
+      case 2:
+        return "naranja";
+      case 3:
+        return "verde";
     }
-    inicializar() {
-        btnEmpezar.classList.add('hide')
-        this.level = 1
-        this.colores = {
-            celeste,
-            violeta,
-            naranja,
-            verde
-        }
+  }
+
+  iluminarSecuencia() {
+    for (let i = 0; i < this.level; i++) {
+        // no se puede usar var, ya que la secuencia siempre se reescribe a si mismo
+      const color = this.transformarNumeroAcolor(this.secuencia[i]);
+      setTimeout(() => this.iluminarColor(color), 1000 * i);
     }
-    generarSecuencia() {
-        this.generarSecuencia = new Array(10).fill(0).map( n => Math.floor(Math.random() * (5 - 1) + 1))
-    }
+  }
+  iluminarColor(color) {
+    this.colores[color].classList.add("light");
+    setTimeout(() => this.apagarColor(color), 350);
+  }
+  apagarColor(color) {
+    this.colores[color].classList.remove("light");
+  }
 }
 
-function empezarJuego () {
-    console.log('inicia el juego')
-    window.juego = new Juego()
+function empezarJuego() {
+  console.log("inicia el juego");
+  window.juego = new Juego();
 }
