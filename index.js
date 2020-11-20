66cee1ff37e6ed1c20534a78e4c806f45045f80e
Songer <ramosger_14@hotmail.com>
2020-11-20 09:40:51 -0600

    funciones para Obtener Input del usuario

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ class Juego {
     this.siguienteNivel();
   }
   inicializar() {
+    this.elegirColor = this.elegirColor.bind(this)
     btnEmpezar.classList.add("hide");
     this.level = 1
     this.colores = {
@@ -27,6 +28,7 @@ class Juego {
   }
   siguienteNivel() {
     this.iluminarSecuencia();
+    this.agregarEventosClick()
   }
   transformarNumeroAcolor(numero) {
     switch (numero) {
@@ -54,6 +56,16 @@ class Juego {
   }
   apagarColor(color) {
     this.colores[color].classList.remove("light");
+  }
+  agregarEventosClick() {
+    this.colores.celeste.addEventListener('click', this.elegirColor)
+    this.colores.violeta.addEventListener('click', this.elegirColor)
+    this.colores.naranja.addEventListener('click', this.elegirColor)
+    this.colores.verde.addEventListener('click', this.elegirColor)
+  }
+  elegirColor(ev){
+    console.log(ev)
+    console.log(this)
   }
 }
 
